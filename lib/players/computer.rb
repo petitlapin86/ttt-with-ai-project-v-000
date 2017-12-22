8f4a86ddef65bb3b759b1e1a21b3c4ef2250ebd4
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-22 18:29:39 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,7 +3,7 @@ module Players
     class Computer < Player
 
           def move(board) # returns a valid position for the computer to move
-            move = nil 
+            move = nil
             
           end
 
